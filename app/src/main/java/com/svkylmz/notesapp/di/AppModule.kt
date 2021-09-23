9449bbfc300c47bb5a7b9228ef7de40d08f8acd5
svkylmz <Ylmazsevket@gmail.com>
2021-09-23 19:19:41 +0300

    Setup dagger-hilt dependency injection library

---
+++ app/src/main/java/com/svkylmz/notesapp/di/AppModule.kt
@@ -0,0 +1,45 @@
+package com.svkylmz.notesapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.svkylmz.notesapp.feature_note.data.data_source.NoteDatabase
+import com.svkylmz.notesapp.feature_note.data.data_source.NoteDatabase.Companion.DATABASE_NAME
+import com.svkylmz.notesapp.feature_note.data.repository.NoteRepositoryImpl
+import com.svkylmz.notesapp.feature_note.domain.repository.NoteRepository
+import com.svkylmz.notesapp.feature_note.domain.use_case.DeleteNote
+import com.svkylmz.notesapp.feature_note.domain.use_case.GetNotes
+import com.svkylmz.notesapp.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository)
+        )
+    }
+}
